adf8efe5cd25b80742010c0a5043cbb6fb637eee
Prashaxt <147325777+Prashaxt@users.noreply.github.com>
2025-01-20 16:34:01 +0530

    added game name api

--- frontend/src/components/Intro.jsx
+++ frontend/src/components/Intro.jsx
@@ -1,16 +1,19 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom' 
 import './Intro.css'
 import { useNavigate } from 'react-router-dom';
+import games from '../assets/games.json'
 
 const Intro = () => {
   const token = localStorage.getItem('token');
 
-  const [GameName, setGameName] = React.useState('Game Name')
+  const [gameList, setGameList] = React.useState([])
   const [Category, setCategory] = React.useState('Category')
 
-  
+  useEffect(()=>{
+    setGameList(games)
+  },[])
 
 
   const navigate = useNavigate();
@@ -54,42 +57,24 @@ const Intro = () => {
     
     <div className="section-gradient"></div>
     <div className="sample-games">
-              <h1>Explore Games</h1>
+              <h1>Explore Categories</h1>
         <div className='game-list'>
-          <div className="gameCard">
-            <div className="cardImage"><img src="https://images.gog-statics.com/93ed2f65c201ee3bb0323de19946175bb0c4e4abda5b637574064c89c0cd12af.jpg" alt="game image" /></div>
-            <h3>{GameName}</h3>
-          </div>
-          <div className="gameCard">
-            <div className="cardImage"><img src="https://preview.redd.it/halo-5-on-pc-v0-vbj9ktw2z4cc1.jpeg?auto=webp&s=d3ee559a6e6ff499f87c1d8b1108ec4c3f56a03a" alt="game image" /></div>
-            <h3>{GameName}</h3>
-          </div>
-          <div className="gameCard">
-            <div className="cardImage"><img src="https://blz-contentstack-images.akamaized.net/v3/assets/bltf408a0557f4e4998/blte7acaf850e5d532d/67576d548c2c1d7026faad29/OW2_2024_S14-Launch-BNET_SHOP-browsingcard_1920x1080.png" alt="game image" /></div>
-            <h3>{GameName}</h3>
-          </div>
-          <div className="gameCard">
-            <div className="cardImage"><img src="https://preview.redd.it/halo-5-on-pc-v0-vbj9ktw2z4cc1.jpeg?auto=webp&s=d3ee559a6e6ff499f87c1d8b1108ec4c3f56a03a" alt="game image" /></div>
-            <h3>{GameName}</h3>
-          </div>
-          <div className="gameCard">
-            <div className="cardImage"><img src="https://blz-contentstack-images.akamaized.net/v3/assets/bltf408a0557f4e4998/blte7acaf850e5d532d/67576d548c2c1d7026faad29/OW2_2024_S14-Launch-BNET_SHOP-browsingcard_1920x1080.png" alt="game image" /></div>
-            <h3>{GameName}</h3>
-          </div>
-          <div className="gameCard">
-            <div className="cardImage"><img src="https://preview.redd.it/halo-5-on-pc-v0-vbj9ktw2z4cc1.jpeg?auto=webp&s=d3ee559a6e6ff499f87c1d8b1108ec4c3f56a03a" alt="game image" /></div>
-            <h3>{GameName}</h3>
-          </div>
-          <div className="gameCard">
-            <div className="cardImage"><img src="https://blz-contentstack-images.akamaized.net/v3/assets/bltf408a0557f4e4998/blte7acaf850e5d532d/67576d548c2c1d7026faad29/OW2_2024_S14-Launch-BNET_SHOP-browsingcard_1920x1080.png" alt="game image" /></div>
-            <h3>{GameName}</h3>
-          </div>
+
+        {gameList.map((game, index) => (
+            <div key={index} className="gameCard">
+              <div className="cardImage">
+                <img src="https://preview.redd.it/halo-5-on-pc-v0-vbj9ktw2z4cc1.jpeg?auto=webp&s=d3ee559a6e6ff499f87c1d8b1108ec4c3f56a03a"  alt={game.game_name} />
+              </div>
+              <h3>{game.game_name}</h3>
+            </div>
+          ))}
+          
      
           
         </div>
       <div className="section-gradient-end"></div>
         <div className="categories">
-          <h1>Categories</h1>
+          <h1>Games</h1>
           <div className="category-list">
             <div className="categoryCard">
               <div className="categoryImage"><img src="https://images.gog-statics.com/93ed2f65c201ee3bb0323de19946175bb0c4e4abda5b637574064c89c0cd12af.jpg" alt="category image" /></div>
