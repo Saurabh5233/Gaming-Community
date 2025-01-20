da7ee9fd68ceb61b5a32b9ab8606f85dbbe4900a
Prashaxt <147325777+Prashaxt@users.noreply.github.com>
2025-01-20 19:48:18 +0530

    added dynamic functions

--- frontend/src/components/Intro.jsx
+++ frontend/src/components/Intro.jsx
@@ -4,15 +4,17 @@ import { Link } from 'react-router-dom'
 import './Intro.css'
 import { useNavigate } from 'react-router-dom';
 import games from '../assets/games.json'
+import gamesCategory from '../assets/gamesCategory.json'
 
 const Intro = () => {
   const token = localStorage.getItem('token');
 
   const [gameList, setGameList] = React.useState([])
-  const [Category, setCategory] = React.useState('Category')
+  const [categoryList, setCategoryList] = React.useState([])
 
   useEffect(()=>{
     setGameList(games)
+    setCategoryList(gamesCategory)
   },[])
 
 
@@ -57,69 +59,32 @@ const Intro = () => {
     
     <div className="section-gradient"></div>
     <div className="sample-games">
-              <h1>Explore Categories</h1>
+              <h1>Game Category</h1>
         <div className='game-list'>
 
-        {gameList.map((game, index) => (
+        {categoryList.map((category, index) => (
             <div key={index} className="gameCard">
               <div className="cardImage">
-                <img src="https://preview.redd.it/halo-5-on-pc-v0-vbj9ktw2z4cc1.jpeg?auto=webp&s=d3ee559a6e6ff499f87c1d8b1108ec4c3f56a03a"  alt={game.game_name} />
+                <img src="https://preview.redd.it/halo-5-on-pc-v0-vbj9ktw2z4cc1.jpeg?auto=webp&s=d3ee559a6e6ff499f87c1d8b1108ec4c3f56a03a"  alt={category.category} />
               </div>
-              <h3>{game.game_name}</h3>
+              <h3>{category.category}</h3>
             </div>
           ))}
-          
-     
           
         </div>
       <div className="section-gradient-end"></div>
         <div className="categories">
           <h1>Games</h1>
           <div className="category-list">
-            <div className="categoryCard">
-              <div className="categoryImage"><img src="https://images.gog-statics.com/93ed2f65c201ee3bb0323de19946175bb0c4e4abda5b637574064c89c0cd12af.jpg" alt="category image" /></div>
-              <h3>{Category}</h3>
+
+          {gameList.map((game, index) => (
+            <div key={index} className="categoryCard">
+              <div className="categoryImage">
+                <img src="https://preview.redd.it/halo-5-on-pc-v0-vbj9ktw2z4cc1.jpeg?auto=webp&s=d3ee559a6e6ff499f87c1d8b1108ec4c3f56a03a"  alt={game.game_name} />
+              </div>
+              <h3>{game.game_name}</h3>
             </div>
-            <div className="categoryCard">
-              <div className="categoryImage"><img src="https://images.gog-statics.com/93ed2f65c201ee3bb0323de19946175bb0c4e4abda5b637574064c89c0cd12af.jpg" alt="category image" /></div>
-              <h3>{Category}</h3>
-            </div>
-            <div className="categoryCard">
-              <div className="categoryImage"><img src="https://images.gog-statics.com/93ed2f65c201ee3bb0323de19946175bb0c4e4abda5b637574064c89c0cd12af.jpg" alt="category image" /></div>
-              <h3>{Category}</h3>
-            </div>
-            <div className="categoryCard">
-              <div className="categoryImage"><img src="https://images.gog-statics.com/93ed2f65c201ee3bb0323de19946175bb0c4e4abda5b637574064c89c0cd12af.jpg" alt="category image" /></div>
-              <h3>{Category}</h3>
-            </div>
-            <div className="categoryCard">
-              <div className="categoryImage"><img src="https://images.gog-statics.com/93ed2f65c201ee3bb0323de19946175bb0c4e4abda5b637574064c89c0cd12af.jpg" alt="category image" /></div>
-              <h3>{Category}</h3>
-            </div>
-            <div className="categoryCard">
-              <div className="categoryImage"><img src="https://images.gog-statics.com/93ed2f65c201ee3bb0323de19946175bb0c4e4abda5b637574064c89c0cd12af.jpg" alt="category image" /></div>
-              <h3>{Category}</h3>
-            </div>
-            <div className="categoryCard">
-              <div className="categoryImage"><img src="https://images.gog-statics.com/93ed2f65c201ee3bb0323de19946175bb0c4e4abda5b637574064c89c0cd12af.jpg" alt="category image" /></div>
-              <h3>{Category}</h3>
-            </div>
-            <div className="categoryCard">
-              <div className="categoryImage"><img src="https://images.gog-statics.com/93ed2f65c201ee3bb0323de19946175bb0c4e4abda5b637574064c89c0cd12af.jpg" alt="category image" /></div>
-              <h3>{Category}</h3>
-            </div>
-            <div className="categoryCard">
-              <div className="categoryImage"><img src="https://images.gog-statics.com/93ed2f65c201ee3bb0323de19946175bb0c4e4abda5b637574064c89c0cd12af.jpg" alt="category image" /></div>
-              <h3>{Category}</h3>
-            </div>
-            <div className="categoryCard">
-              <div className="categoryImage"><img src="https://images.gog-statics.com/93ed2f65c201ee3bb0323de19946175bb0c4e4abda5b637574064c89c0cd12af.jpg" alt="category image" /></div>
-              <h3>{Category}</h3>
-            </div>
-            <div className="categoryCard">
-              <div className="categoryImage"><img src="https://images.gog-statics.com/93ed2f65c201ee3bb0323de19946175bb0c4e4abda5b637574064c89c0cd12af.jpg" alt="category image" /></div>
-              <h3>{Category}</h3>
-            </div>
+          ))}
             
           </div>
             <div className="section-gradient-end"></div>
